1dbe91f99c9ef69cde26b73b55f99127d5b60d0d
arturs <arturshugo4@gmail.com>
2025-04-03 16:44:55 +0300

    Removed boilerplate hence it was wrong cause it uses it from layouts

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,38 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Product</title>
-</head>
-<body>
     @extends('layouts.app', ['title' => 'Izveidot produktu'])
 
     @section('content')
-        <h1>Izveidot produktu</h1>
+    <h1>Izveidot produktu</h1>
 
-        <form method="post" action="{{ route('products.store') }}">
-            @csrf
-            <div class="mb-3">
-                <label for="nameInput" class="form-label">Nosaukums</label>
-                <input type="text" class="form-control" id="nameInput" name="name"value="{{ old('name') }}">
-            </div>
-            <div class="mb-3">
-                <label for="qtyInput" class="form-label">Daudzums</label>
-                <input type="text" class="form-control" id="qtyInput" name="qty" value="{{ old('qty') }}">
-            </div>
-            <div class="mb-3">
-                <label for="descriptionInput" class="form-label">Apraksts</label>
-                <input type="text" class="form-control" id="descriptionInput" name="description" value="{{ old('description') }}">
-            </div>
-            <div class="mb-3">
-                <label for="priceInput" class="form-label">Cena</label>
-                <input type="text" class="form-control" id="priceInput" name="price" value="{{ old('price') }}">
-            </div>
-            <div class="mb-3">
-                <input type="submit" class="btn btn-primary" value="Saglabāt">
-            </div>
-        </form>
+    <form method="post" action="{{ route('products.store') }}">
+        @csrf
+        <div class="mb-3">
+            <label for="nameInput" class="form-label">Nosaukums</label>
+            <input type="text" class="form-control" id="nameInput" name="name"value="{{ old('name') }}">
+        </div>
+        <div class="mb-3">
+            <label for="qtyInput" class="form-label">Daudzums</label>
+            <input type="text" class="form-control" id="qtyInput" name="qty" value="{{ old('qty') }}">
+        </div>
+        <div class="mb-3">
+            <label for="descriptionInput" class="form-label">Apraksts</label>
+            <input type="text" class="form-control" id="descriptionInput" name="description" value="{{ old('description') }}">
+        </div>
+        <div class="mb-3">
+            <label for="priceInput" class="form-label">Cena</label>
+            <input type="text" class="form-control" id="priceInput" name="price" value="{{ old('price') }}">
+        </div>
+        <div class="mb-3">
+            <input type="submit" class="btn btn-primary" value="Saglabāt">
+        </div>
+    </form>
     @endsection
-</body>
-</html>
