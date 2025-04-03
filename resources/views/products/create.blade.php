877bf812e6935bd2d6845ad87809cb774e0eb859
arturs <arturshugo4@gmail.com>
2025-04-03 16:17:05 +0300

    Added a new div in create view. (Price)

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -26,6 +26,10 @@
                 <input type="text" class="form-control" id="descriptionInput" name="description" value="{{ old('description') }}">
             </div>
             <div class="mb-3">
+                <label for="priceInput" class="form-label">Cena</label>
+                <input type="text" class="form-control" id="priceInput" name="price" value="{{ old('price') }}">
+            </div>
+            <div class="mb-3">
                 <input type="submit" class="btn btn-primary" value="Saglabāt">
             </div>
         </form>
